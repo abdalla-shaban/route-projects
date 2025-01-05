7361d5d3b93f35f7b2e76ce3b2d9d3239987ca7c
Abdullah Madkour <abdallashaban584@gmail.com>
2025-01-06 00:29:03 +0200

    add src main files

---
+++ simple-portfiolio-(react)/src/App.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Contact from "./components/Contact/Contact";
+
+const routers = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={routers}></RouterProvider>;
+}
+
+export default App;
